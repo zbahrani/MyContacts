2aeed1f7a0282402e834022560b8274e6bd2f874
ACER <ACER@DESKTOP-AO608KQ>
2025-07-27 19:30:02 +0330

    Completing the Insert Method

--- MyContacts/Services/ContactsRepository.cs
+++ MyContacts/Services/ContactsRepository.cs
@@ -17,7 +17,29 @@ namespace MyContacts.Services
 
         public bool Insert(string name, string family, string mobile, string email, int age, string address)
         {
-            throw new NotImplementedException();
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                string query = "Insert Into MyContacts (Name,Family,Email,Age,Mobile,Address) values (@Name,@Family,@Email,@Age,@Mobile,@Address)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@Family", family);
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Age", age);
+                command.Parameters.AddWithValue("@Mobile", mobile);
+                command.Parameters.AddWithValue("@Address", address);
+                connection.Open();
+                command.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
 
         public DataTable SelcetRow(int contactId)
