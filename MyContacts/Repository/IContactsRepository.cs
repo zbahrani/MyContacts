c433d588b128ac6a0a3f296eceab12c24984a5ac
ACER <ACER@DESKTOP-AO608KQ>
2025-07-27 11:16:19 +0330

    Create Repository

---
+++ MyContacts/Repository/IContactsRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace MyContacts.Repository
+{
+    interface IContactsRepository
+    {
+        DataTable SelectAll();
+        DataTable SelcetRow(int contactId);
+        bool Insert(string name, string family, string mobile, string email, int age, string address);
+        bool Update(int contactId, string name, string family, string mobile, string email, int age, string address);
+        bool Delete(int contactId);
+    }
+}
